05c31f8c7910b2ea21777628bb4ef96b714bec82
J Camilo Rpo V <juan.restrepov@upb.edu.co>
2020-08-14 02:15:17 -0500

    Se agregan los modelos para la persistencia de datos

---
+++ UPBEats/Models/SeedData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UPBEats.Data;
+
+namespace UPBEats.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new UPBEatsContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<UPBEatsContext>>()))
+            {
+                // Buscando cualquier rol
+                if (context.Rol.Any())
+                    return;   // Ya se inicializó la BD
+
+                //Roles agregar
+                context.Rol.AddRange(
+                    new Rol { Nombre = "Comprador" },
+                    new Rol { Nombre = "Vendedor" }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
