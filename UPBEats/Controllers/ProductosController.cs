f20cb1730a28ded383f5ecc8e2f496d1f10d023e
J Camilo Rpo V <juan.restrepov@upb.edu.co>
2020-08-24 13:52:38 -0500

    Modificar productos

--- UPBEats/Controllers/ProductosController.cs
+++ UPBEats/Controllers/ProductosController.cs
@@ -78,14 +78,10 @@ namespace UPBEats.Controllers
 
             if (ModelState.IsValid)
             {
-                //Desde la vista se envia la foto en base 64
-                producto.Foto = producto.Foto.ToArray<byte>();
-
                 _context.Add(producto);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            //ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "Apellido", producto.UsuarioId);
             return View(producto);
         }
 
@@ -176,46 +172,6 @@ namespace UPBEats.Controllers
             {
                 return RedirectToAction("Principal", "Home");
             }
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Details(int id, [Bind("Id,Nombre,Descripcion,Precio,FileFoto,UsuarioId")] Producto producto)
-        {
-            if (id != producto.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                if (producto.FileFoto != null)
-                {
-                    var stream = new MemoryStream();
-                    await producto.FileFoto.CopyToAsync(stream);
-                    producto.Foto = stream.ToArray();
-                }
-                //Si no se selecciona foto, se deja la que tenia
-
-                try
-                {
-                    _context.Update(producto);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductoExists(producto.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return Details(id).Result;
-            }
-            return Details(id).Result;
         }
 
         // GET: Productos/Delete/5
